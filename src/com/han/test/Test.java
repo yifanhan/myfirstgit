7b24676b3e7c64ca9c011ac3437558904b24d88e
yifanhan <842071912@qq.com>
2014-11-12 14:11:04 +0800

    zzz

---
+++ src/com/han/test/Test.java
@@ -0,0 +1,25 @@
+package com.han.test;
+
+public class Test {
+	/**
+	 * @descript 
+	 * @author hanqingsong
+	 * @version 1.0
+	 * @date 2014年11月12日下午2:04:35
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test.main()");
+	}
+	/**
+	 * @descript 
+	 * @author hanqingsong
+	 * @version 1.0
+	 * @date 2014年11月12日下午2:05:19
+	 * @param i
+	 * @return
+	 */
+	public String tt(int i){
+		return "";
+	}
+}
